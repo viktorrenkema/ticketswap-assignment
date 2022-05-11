f4becc73069f5f6b60af45c3feab0aba10faf640
viktorrenkema <vrenkema@gmail.com>
2022-05-11 13:15:54 +0200

    Create additional page to test for deploying

---
+++ pages/test.js
@@ -0,0 +1,75 @@
+// Larger libraries
+import React from 'react'
+import styled from '@emotion/styled'
+
+// Components
+import { space, sizes, Card, Spinner } from '@ticketswap/solar'
+import { WarningRounded } from '@ticketswap/solar/icons'
+import AllEvents from '~/components/AllEvents'
+import Cover from '~/components/Cover'
+import Footer from '~/components/Footer'
+
+// Utilities
+import { useQuery } from '@apollo/client'
+import { useDebounce } from '~/lib/useDebounce'
+import getAllEvents from '~/graphql/queries/getAllEvents'
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: ${space[16]};
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+const SearchPageContainer = styled.main`
+  padding: 0 ${space[16]};
+  margin: ${space[16]} auto;
+  max-width: ${sizes.laptop}px;
+  display: flex;
+  flex-direction: column;
+  gap: ${space[32]};
+`
+
+const Search = () => {
+  const [query, setQuery] = React.useState('')
+  const debouncedSearch = useDebounce(query, 500)
+
+  const { loading, data, refetch } = useQuery(getAllEvents, {
+    variables: {
+      name: '',
+    },
+  })
+
+  React.useEffect(
+    () => {
+      refetch({ name: debouncedSearch })
+    },
+    [debouncedSearch, refetch] // Only call effect if debounced search term changes
+  )
+
+  if (loading) {
+    return (
+      <Wrapper>
+        <Spinner />
+      </Wrapper>
+    )
+  }
+
+  const { allEvents } = data
+
+  return (
+    <>
+      <Cover page="search" setQuery={setQuery} />
+      <SearchPageContainer>
+        <Card
+          leftAdornment={<WarningRounded size={24} />}
+          title="Teeeest"
+          description="."
+        />
+      </SearchPageContainer>
+      <Footer />
+    </>
+  )
+}
+
+export default Search
