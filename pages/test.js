568ea4f71a694b69e0916126b125e4d370c5dd13
viktorrenkema <vrenkema@gmail.com>
2022-05-11 13:19:38 +0200

    Fix errors 2

--- pages/test.js
+++ pages/test.js
@@ -3,23 +3,10 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 // Components
-import { space, sizes, Card, Spinner } from '@ticketswap/solar'
+import { space, sizes, Card } from '@ticketswap/solar'
 import { WarningRounded } from '@ticketswap/solar/icons'
-import AllEvents from '~/components/AllEvents'
-import Cover from '~/components/Cover'
+
 import Footer from '~/components/Footer'
-
-// Utilities
-import { useQuery } from '@apollo/client'
-import { useDebounce } from '~/lib/useDebounce'
-import getAllEvents from '~/graphql/queries/getAllEvents'
-
-const Wrapper = styled.div`
-  display: flex;
-  gap: ${space[16]};
-  flex-wrap: wrap;
-  justify-content: center;
-`
 
 const SearchPageContainer = styled.main`
   padding: 0 ${space[16]};
